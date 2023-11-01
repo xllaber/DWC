9f3df9a9e09e1fa21088c2263127883d2cf8973f
Ximo Llacer <xllaber7@gmail.com>
2023-11-01 20:43:43 +0100

    Carito update

--- Carrito/js/carrito.js
+++ Carrito/js/carrito.js
@@ -5,16 +5,31 @@ class Carrito{
 	}
 						
 	anyadeArticulo(articulo){
-		}			
+		// let existe = this.articulos.find(a => a.codigo === articulo.codigo);
+		if (this.articulos.indexOf(articulo) !== -1) {
+			articulo.unidades++;
+		} else {
+			console.log(this);
+			this.articulos.push(articulo);
+		}
+	}
 				
 	borraArticulo(codigo){		
+		let articuloIdx = this.articulos.findIndex(a => a.codigo === codigo);
+		this.articulos.splice(articuloIdx, 1);
+		this.verCarrito();
 	}
 	
 	modificaUnidades(codigo, n){
+		let articulo = this.articulos.find(a => a.codigo === codigo);
+		console.log(articulo);
+		(n === 1) ? articulo.unidades++ : (n === -1) ? articulo.unidades-- : undefined;
+		this.verCarrito();
 	}	
 			
 	verCarrito(){
 		let dialogo = document.getElementById("miDialogo");
+		dialogo.showModal();
 		let dialogContent = document.getElementById("dialogContent");
 		document.getElementById("idPedido").innerHTML = `${this.id}`;
 		// let table = document.createElement("table");
@@ -33,19 +48,29 @@ class Carrito{
 									`;
 		this.articulos.forEach(a => {
 			dialogContent.innerHTML += `<tr>
-											<td><img src="../assets/img/${a.codigo}" alt="${a.nombre}"></td>
+											<td><img src="../assets/img/${a.codigo}.jpg" width="40px" height="40px" alt="${a.nombre}"></td>
 											<td>${a.nombre}</td>
 											<td>${a.descripcion}</td>
 											<td>${a.precio}</td>
 											<td>${a.unidades}</td>
 											<td>${a.unidades * a.precio}</td>
 											<td>
-												<button class="btn-success">+</button>
-												<button class="btn-warning">-</button>
-												<button class="btn-danger">Eliminar Producto</button>
+												<button id="incremento" class="btn-success">+</button>
+												<button id="decremento" class="btn-warning">-</button>
+												<button id="borrar" class="btn-danger">Eliminar Producto</button>
 											</td>
 										</tr>`
+			// document.getElementById("incremento").addEventListener("click", () => this.modificaUnidades(a.codigo, 1));
+			// document.getElementById("decremento").addEventListener("click", () => this.modificaUnidades(a.codigo, -1));
+			// document.getElementById("borrar").addEventListener("click", () => this.borraArticulo(a.codigo));
 		})
 		dialogContent.innerHTML += "</tbody></table>";
-	}			
+		// let btnsIncremento = Array.from(document.getElementsByClassName("btn-success"));
+		// let btnsDecremento = Array.from(document.getElementsByClassName("btn-warning"));
+		// let btnsEliminar = Array.from(document.getElementsByClassName("btn-danger"));
+		//
+		// btnsIncremento.forEach(b => b.addEventListener("click", () => this.modificaUnidades(a.codigo, 1)));
+		// btnsDecremento.forEach(b => b.addEventListener("click", () => this.modificaUnidades(a.codigo, -1)));
+		// btnsEliminar.forEach(b => b.addEventListener("click", () => this.borraArticulo(a.codigo)));
+	}
 }
--- Carrito/js/tienda.js
+++ Carrito/js/tienda.js
@@ -2,6 +2,73 @@ criterios=["Sin ordenar","Ascendente por precio", "Descendente por precio"]
 let carritoGeneral = {};
 let carritos = [];
 
+function creaListaCriterios(){
+	let criteriosOrdenacion = document.getElementById("criteriosOrdenacion");
+	criterios.forEach(c => {
+		let option = document.createElement("option");
+		option.value = c;
+		option.textContent = c;
+		criteriosOrdenacion.appendChild(option);
+	})
+	criteriosOrdenacion.addEventListener("change", () => pintaArticulos(criteriosOrdenacion.value));
+}
+
+function pintaArticulos(orden){
+	let contenedor = document.getElementById("contenedor");
+	contenedor.innerHTML = "";
+	let listaOrdenada = [];
+	let copia = [...listaArticulos];
+
+	if (orden === criterios[0]){
+		listaOrdenada = listaArticulos;
+	} else if (orden === criterios[1]){
+		listaOrdenada = copia.sort((a, b) => a.precio - b.precio);
+	} else if (orden === criterios[2]) {
+		listaOrdenada = copia.sort((a, b) => b.precio - a.precio);
+	}
+
+	listaOrdenada.forEach( a => {
+		console.log(a);
+		contenedor.innerHTML += `<div class='col'>
+									<div class="card">
+										<img src="../assets/img/${a.codigo}.jpg" alt="${a.nombre}">
+										<div class="card-body">
+											<h5 class="card-title">${a.nombre}</h5>
+											<p class="card-text">${a.descripcion}</p>
+											<b><p class="card-text text-center">${a.precio}</p></b>
+										</div>
+										<button id="${a.codigo}" class="btn-success">COMPRAR</button>
+									</div>
+								</div>
+								`
+		// debugger;
+		// document.getElementById(`${a.codigo}`).addEventListener("click", () => ponArticuloEnCarrito(a.codigo));
+	})
+	Array.from(document.getElementsByClassName("btn-success")).forEach( b => {
+		b.addEventListener("click", () => {
+			let articulo =  listaArticulos.find(a => a.codigo === b.id);
+			ponArticuloEnCarrito(articulo)
+		})
+	})
+}
+
+
+function ponArticuloEnCarrito(articulo){
+	// let articulo = listaArticulos.find(a => a.codigo === codigo);
+	console.log(articulo);
+	articulo.unidades = 1;
+	carritoGeneral.anyadeArticulo(articulo);
+}
+
+function verCarro(){
+	carritoGeneral.verCarrito();
+	console.log(carritoGeneral);
+}
+
+function efectuaPedido(){
+
+}
+
 window.onload= () => {
 	creaListaCriterios();
 	pintaArticulos(criterios[0]);
@@ -13,60 +80,12 @@ window.onload= () => {
 	let carritoImg = document.getElementById("carritoImg");
 	carritoImg.addEventListener("click", () => verCarro());
 
+	// let btnsIncremento = Array.from(document.getElementsByClassName("btn-success"));
+	// let btnsDecremento = Array.from(document.getElementsByClassName("btn-warning"));
+	// let btnsEliminar = Array.from(document.getElementsByClassName("btn-danger"));
+	//
+	// btnsIncremento.forEach(b => b.addEventListener("click", () => this.modificaUnidades(a.codigo, 1)));
+	// btnsDecremento.forEach(b => b.addEventListener("click", () => this.modificaUnidades(a.codigo, -1)));
+	// btnsEliminar.forEach(b => b.addEventListener("click", () => this.borraArticulo(a.codigo)));
+
 }
-
-	function creaListaCriterios(){
-		let criteriosOrdenacion = document.getElementById("criteriosOrdenacion");
-		criterios.forEach(c => {
-			let option = document.createElement("option");
-			option.value = c;
-			option.textContent = c;
-			criteriosOrdenacion.appendChild(option);
-		})
-		criteriosOrdenacion.addEventListener("change", () => pintaArticulos(criteriosOrdenacion.value));
-	}
-
-	function pintaArticulos(orden){
-		let contenedor = document.getElementById("contenedor");
-		contenedor.innerHTML = "";
-		let listaOrdenada = [];
-		let copia = [...listaArticulos];
-
-		if (orden === criterios[0]){
-			listaOrdenada = listaArticulos;
-		} else if (orden === criterios[1]){
-			listaOrdenada = copia.sort((a, b) => a.precio - b.precio);
-		} else if (orden === criterios[2]) {
-			listaOrdenada = copia.sort((a, b) => b.precio - a.precio);
-		}
-
-		listaOrdenada.forEach( a => {
-			contenedor.innerHTML += `<div class='col'>
-										<div class="card">
-											<img src="../assets/img/${a.codigo}.jpg" alt="${a.nombre}">
-											<div class="card-body">
-												<h5 class="card-title">${a.nombre}</h5>
-												<p class="card-text">${a.descripcion}</p>
-												<b><p class="card-text text-center">${a.precio}</p></b>
-											</div>
-											<button id="${a.codigo}" class="btn-success">COMPRAR</button>
-										</div>
-									</div>
-									`
-			document.getElementById(`${a.codigo}`).addEventListener("click", () => ponArticuloEnCarrito(a));
-		})
-	}
-
-
-	function ponArticuloEnCarrito(articulo){
-		carritoGeneral.anyadeArticulo(articulo);
-	}
-
-	function verCarro(){
-		carritoGeneral.verCarrito();
-	}
-
-	function efectuaPedido(){
-
-	}
-
