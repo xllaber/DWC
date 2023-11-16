82096aedb07b3024cb59510fa3fb0d8e279fe674
Ximo Llacer <xllaber7@gmail.com>
2023-11-16 13:43:12 +0100

    Chained then()

--- promesas/index.js
+++ promesas/index.js
@@ -9,15 +9,16 @@ function get(entidad, id) {
         req.responseType = "json";
         req.onload = function() {
             if (req.status === 200) {
-                console.log(req.response);
-                let req2 = new XMLHttpRequest();
-                req2.open("GET", url + "actors/" + req.response.actorId);
-                req2.responseType = "json";
-                req2.send();
-                req2.onload = () => {
-                    console.log(req2.response.name);
-                }
-                // resolve(req.response); // Resolve the promise with the response text
+                // console.log(req.response);
+                // let req2 = new XMLHttpRequest();
+                // req2.open("GET", url + "actors/" + req.response.actorId);
+                // req2.responseType = "json";
+                // req2.send();
+                // req2.onload = () => {
+                //     // console.log(req2.response.name);
+                //     promise();
+                // }
+                resolve(req.response); // Resolve the promise with the response text
             }
             else {
                 reject(Error(req.statusText)); // HTTP Error reject with statusText
@@ -33,8 +34,8 @@ function get(entidad, id) {
 
 let entidad = prompt("Dime la entidad", "movies");
 let id = parseInt(prompt("Dime id de la pelicula"));
-get(entidad, id).then(response => response.forEach(r => console.log(r))).catch(error => console.log(error));
+// get(entidad, id).then(response => response.forEach(r => console.log(r))).catch(error => console.log(error));
 
-// let promise = new Promise((resolve, reject) => {
-//     get(entidad, id).then(pelicula => get("actors", pelicula.actorId).catch(() => console.log("Error")));
-// })
+let promise = new Promise((resolve, reject) => {
+    get(entidad, id).then(pelicula => get("actors", pelicula.actorId).then(a => console.log(a)).catch(() => console.log("Error")));
+})
